dfff434d6d90e35ad82cdb0f325277dbf1fd1b9f
Azzahraa16 <azzahraara0416@gmail.com>
2024-11-10 00:50:16 +1300

    index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Perpustakaan SMK Negeri 1 Cimahi</title>
+        <title>Perpustakaan</title>
 
         <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/css/bootstrap.css">
         <script type="text/javascript" src="{{ asset('assets') }}/js/jquery-3.2.1.min.js"></script>
